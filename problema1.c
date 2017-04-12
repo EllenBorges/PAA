24846fa48da0785fabcabfe09b2506e306ccd450
EllenBorges <ellen.pboliveira@gmail.com>
2017-04-12 13:51:56 -0300

    Update problema1.c

--- problema1.c
+++ problema1.c
@@ -10,27 +10,28 @@ typedef struct {
 	int tamanho;
 
 }TItem;
-/*
-int Carrega(TItem **A)
-{
-	int i, n;
 
-	scanf("%d", &n);
+void Carrega(TItem *A, int k){
+    int j, p,tam;
 
-	if (n > 0) {
-		(*A) = (TItem *) malloc(n * sizeof(TItem));
-		for (i = 0; i < n ; i++)
-			scanf("%d", &(*A)[i].Chave);
-			(*A)[i].tam = n;
+	for (j = 0; j < k; j++){
+        scanf("%d", &tam);
+        A[j].tamanho = tam;
+        if (tam == 0){
+            A[j].min = -1;
+            A[j].vetor = NULL;
+        }else{
+            int *temp = (int*)malloc((tam)*sizeof(int));
+            for (p = 0; p < tam; p++ ){
+                scanf("%d", &temp[p]);
+            }
+            A[j].vetor = temp;
+            A[j].min = 0;
+        }
+    }
 
-	}
-	else
-		(*A) = NULL;
-    printf("%d",(*A)[0]->tam);
-  //  Imprime((*A),();
-	return n;
 }
-
+/*
 void Libera(TItem **A)
 {
 	if ((*A) != NULL) {
@@ -106,22 +107,27 @@ void heapSort(TItem *A, int n)
 }
 
 */
-void ImprimeVetor(int *A, int n)
+void ImprimeVetor(int *A, int tam)
 {
 	int i;
-	if (n > 0) {
+	if (tam > 0) {
 		printf("%d", A[0]);
-		for (i = 1; i < n; i++)
+		for (i = 1; i < tam; i++)
 			printf(" %d", A[i]);
 		printf("\n");
 	}
 }
 
+void Imprime(TItem *A, int k){
+    int j;
+    for (j = 0; j < k; j++){
+        ImprimeVetor(A[j].vetor, A[j].tamanho);
+    }
+}
 
 int main(){
 
-	int k,i,j,n,p;
-	int tam;
+	int k,i,n,j;
 
 	TItem *A;
 
@@ -129,27 +135,9 @@ int main(){
 
 	scanf("%d %d",&k, &i);
 	A = (TItem*)malloc((k)*sizeof(TItem));
+	Carrega(A,k);
+    Imprime(A,k);
 
-
-    for (j = 0; j < k; j++){
-        scanf("%d", &tam);
-        A[j].tamanho = tam;
-        if (tam == 0){
-            A[j].min = -1;
-            A[j].vetor = NULL;
-        }else{
-            int *temp = (int*)malloc((tam)*sizeof(int));
-            for (p = 0; p < tam; p++ ){
-                scanf("%d", &temp[p]);
-            }
-            A[j].vetor = temp;
-            A[j].min = 0;
-        }
-    }
-
-    for (j = 0; j < k; j++){
-        ImprimeVetor(A[j].vetor, A[j].tamanho);
-    }
 
 
 
